eb4f2e5884af1c586343b416f90bb2b4fa454e5d
kettleofketchup <kettleofketchup@users.noreply.github.com>
2025-08-09 08:00:54 -0400

    updated install process

--- scripts/linux/install_terminal_addons.sh
+++ scripts/linux/install_terminal_addons.sh
@@ -6,15 +6,20 @@ curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/bin
 
 
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+WORKING_DIR=$(pwd)
+
+if [ ! -d "$HOME/.fzf" ]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  bash ~/.fzf/install --bin
+else 
+    cd ~/.fzf
+    git pull
+    bash ~/.fzf/install --bin
+fi
 
 
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-
-
-
-bash ~/.fzf/install --bin
-
+cd "$WORKING_DIR"
 
 
 
